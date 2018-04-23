817de6c665a7136a53aded4f64438bc9950fdd7b
Luis <luis.fgbo@gmail.com>
2018-04-23 19:01:35 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './assets/styles/main.css';
+
+import Router from './components/router';
+
+export default () => <Router/>;
---
+++ src/components/screens/home-screen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <section className="home">
+                <header className="bl-header">
+                    <h1 className="bl-header--title">&nbsp;</h1>
+                    <p  className="bl-header--subtitle">&nbsp;</p>
+                </header>
+            </section>
+        );
+    }
+}
